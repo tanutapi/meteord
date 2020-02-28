638155ae72a3e4857b47257fd5ea4cd459a1dd95
Tanut Apiwong <tanutapi@gmail.com>
2020-02-28 16:57:10 +0700

    Update docker repository

--- tests/test_bundle_web.sh
+++ tests/test_bundle_web.sh
@@ -14,9 +14,9 @@ clean
 docker run -d \
     --name web \
     -e ROOT_URL=http://web_app \
-    -e BUNDLE_URL=https://abernix-meteord-tests.s3-us-west-2.amazonaws.com/meteord-test-bundle.tar.gz \
+    -e BUNDLE_URL=https://tanutapi-meteord-tests.s3-us-west-2.amazonaws.com/meteord-test-bundle.tar.gz \
     -p 9090:80 \
-    "abernix/meteord:node-${NODE_VERSION}-base"
+    "tanutapi/meteord:node-${NODE_VERSION}-base"
 
 sleep 120
 
